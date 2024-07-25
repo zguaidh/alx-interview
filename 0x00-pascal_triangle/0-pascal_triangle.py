e8c796633b840756b873a196f280401e8a67972d
habiba zguaid <zguaidhabiba@gmail.com>
2024-07-25 23:19:54 +0100

    task0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle 
+"""
+def pascal_triangle(n):
+    """
+    Pascal's Triangle
+    """
+    if n <= 0:
+        return []
+    else:
+        nums = []
+        for i in range(1, n + 1):
+            C = 1
+            ls = []
+            for j in range(1, i+1):
+                ls.append(C)    
+                C = C * (i - j) // j
+            nums.append(ls)
+        return nums
