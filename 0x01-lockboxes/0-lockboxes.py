042ac0b87cfe1d0b292407fffa1594597a7c9085
zguaidh <zguaidhabiba@gmail.com>
2024-07-31 15:21:22 +0000

    starting Typescript

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+  n = len(boxes)
+  unlocked = [False] * n
+  unlocked[0] = True
+  stack = [0]
+
+  while stack:
+    box = stack.pop()
+    for key in boxes[box]:
+      if key < n and not unlocked[key]:
+        unlocked[key] = True
+        stack.append(key)
+    
+  return all(unlocked)
+
