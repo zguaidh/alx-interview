b916a27cd88f33a741224207bae81a368dc4e439
zguaidh <zguaidhabiba@gmail.com>
2024-07-31 15:38:01 +0000

    Fixing the pystyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 
+
 def canUnlockAll(boxes):
-'''This function determines if all the boxes can be unlocked'''
+    '''This function determines if all the boxes can be unlocked'''
 
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    stack = [0]
 
-  n = len(boxes)
-  unlocked = [False] * n
-  unlocked[0] = True
-  stack = [0]
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                stack.append(key)
 
-  while stack:
-    box = stack.pop()
-    for key in boxes[box]:
-      if key < n and not unlocked[key]:
-        unlocked[key] = True
-        stack.append(key)
-    
-  return all(unlocked)
-
+    return all(unlocked)
