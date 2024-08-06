4487a65756be85c416bd88a94792a2a592780f17
zguaidh <zguaidhabiba@gmail.com>
2024-08-06 17:39:24 +0000

    finished code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,10 +10,8 @@ def minoperations(n):
     """Return the minimum number of operations."""
     if n <= 1:
         return 0
-
     operations = 0
     divisor = 2
-
     while n > 1:
         while n % divisor == 0:
             operations += divisor
