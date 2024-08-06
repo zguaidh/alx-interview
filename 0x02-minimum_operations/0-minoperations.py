862ded595e287d125fd2d197c966df4dda865e3c
zguaidh <zguaidhabiba@gmail.com>
2024-08-06 17:30:22 +0000

    finished code

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''creates a function that takes n as an arguments and returns
+the number of operatiors required to print n characters'''
+
+
+def minOperations(n):
+    '''returns the minimum number of operations'''
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+    return operations
