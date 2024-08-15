7cde8eb953084c67f9104f9803a34fbd25c0f9ae
zguaidh <zguaidhabiba@gmail.com>
2024-08-15 18:04:57 +0000

    task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -39,6 +39,7 @@ def print_stats(status_codes: dict, total_size: int) -> None:
 
 def signal_handler(sig, frame):
     '''Handles Crtl+C signal to print stats before exiting'''
+    print_stats()
     sys.exit(0)
 
 
