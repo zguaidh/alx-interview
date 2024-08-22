047aab5f684df0a5e0e117e3f74ec9d8b5466dc9
zguaidh <zguaidhabiba@gmail.com>
2024-08-22 21:38:47 +0000

    README file

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,4 +9,24 @@ def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding
     """
-    pass
+    num_bytes = 0
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
+
+    for byte in data:
+        mask = 1 << 7
+        if num_bytes == 0:
+            while mask & byte:
+                num_bytes += 1
+                mask >>= 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+        num_bytes -= 1
+    return num_bytes == 0
