f46120cc586e1a006e6a05ef7ec00df49831cb2d
zguaidh <zguaidhabiba@gmail.com>
2024-08-22 21:33:17 +0000

    README file

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for a method that determines if
+a given data set represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding
+    """
+    pass
