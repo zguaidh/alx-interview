27664b353c73e6748babbc16cc71a5dd4ecce6d5
zguaidh <zguaidhabiba@gmail.com>
2024-08-29 19:00:14 +0000

    Nqueens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Module to solve the Nqueens problem"""
+import sys
+
+
+def is_safe(board, row, col, N):
+    """Check if it's safe to place a queen at board[row][col]"""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
+def solve_nqueens(board, col, N):
+    """Use backtracking to solve the N-Queens problem"""
+    if col >= N:
+        solution = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        print(solution)
+        return True
+    res = False
+    for i in range(N):
+        if is_safe(board, i, col, N):
+            board[i][col] = 1
+            res = solve_nqueens(board, col + 1, N) or res
+            board[i][col] = 0
+    return res
+
+
+def main():
+    """Main function to handle input and trigger the solving process"""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    
+    board = [[0] * N for _ in range(N)]
+    solve_nqueens(board, 0, N)
+
+
+if __name__ == "__main__":
+    main()
