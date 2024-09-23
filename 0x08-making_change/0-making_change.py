8c36d44a0b86426952743ecb2cffd59dc5a070a4
habiba zguaid <zguaidhabiba@gmail.com>
2024-09-23 19:16:37 +0100

    task0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Change comes from within
+"""
+
+
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given amount total
+    """
+    if total <= 0:
+        return 0
+
+    sorted_coins = sorted(coins, reverse=True)
+    coins_count = 0
+    remainder = total
+
+    for coin in sorted_coins:
+        if remainder == 0:
+            break
+
+        coin_count = remainder // coin
+        coins_count += coin_count
+        remainder -= coin_count * coin
+
+
+    if remainder > 0:
+        return -1
+
+    return coins_count
+
